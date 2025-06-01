d53748b7cde3c01fd3ebaa78ed38ead535aef378
bankzhy <zhounan515130436@gmail.com>
2025-06-01 23:58:20 +0900

    222

--- jdeo/lc/generate_csv.py
+++ jdeo/lc/generate_csv.py
@@ -15,11 +15,42 @@ def generate_csv():
                 with open(f, "r") as f:
                     all_lines = f.readlines()
                     for index, line in enumerate(all_lines):
+                        if line == "":
+                            continue
                         ll = line.split("	")
                         target_class_name = ll[0].split(".")[-1]
-                        element_l = ll[1]
+                        element_l = ll[1].replace("[", "").replace("]", "")
+                        if ll[0].startswith("org"):
+                            element_l = element_l.split(", org.")
+                        else:
+                            element_l = element_l.split(", com.")
+                        for e in element_l:
+                            entity = e.split("::")[-1]
+                            if "(" in entity and ")" in entity:
+                                method_name = entity.split("(")[0]
+                                if target_class_name in cls_method_dict.keys():
+                                    cls_method_dict[target_class_name].append(method_name)
+                                else:
+                                    cls_method_dict[target_class_name] = [method_name]
+                            else:
+                                field_name = entity.split(" ")[-1]
+                                if target_class_name in cls_field_dict.keys():
+                                    cls_field_dict[target_class_name].append(field_name)
+                                else:
+                                    cls_field_dict[target_class_name] = [field_name]
+
+
+
                         print(element_l)
 
+                for cls in cls_method_dict.keys():
+                    extract_methods = ";".join(cls_method_dict[cls])
+                    extract_fields = ""
+                    if cls in cls_field_dict.keys():
+                        extract_fields = ";".join(cls_field_dict[cls])
+
+                    writer.writerow(dict(zip(file_order,
+                                             [project_name, cls, extract_methods, extract_fields])))
 
 if __name__ == '__main__':
     generate_csv()
